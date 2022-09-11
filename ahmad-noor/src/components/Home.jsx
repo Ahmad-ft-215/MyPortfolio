6f4cd3d9e5dda915f094c66624ea345394bc0b15
Ahmad Noor <99959745+Ahmad-ft-215@users.noreply.github.com>
2022-09-11 11:12:21 +0530

    updated

--- ahmad-noor/src/components/Home.jsx
+++ ahmad-noor/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Ahmad Noor</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Full Stack Developer.</h2>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Full Stack Web Developer.</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           Determinant full-stack web-developer with a specialization in MERN
           stack.Excellent teamwork, self-motivated and curious, with a keen
